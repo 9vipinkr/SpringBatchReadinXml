7a635fa99d7485a9ffe3d05cb454c5e58d563a26
9vipinkr <9vipinkr@gmail.com>
2018-07-17 22:09:34 +0530

    Update ReadingXmlApplication.java
    add comments

--- src/main/java/io/spring/batch/ReadingXmlApplication.java
+++ src/main/java/io/spring/batch/ReadingXmlApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 @EnableBatchProcessing
 public class ReadingXmlApplication {
-
+//add commit
     public static void main(String[] args) {
         SpringApplication.run(ReadingXmlApplication.class, args);
     }
